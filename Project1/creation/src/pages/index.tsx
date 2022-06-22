1b46920406befc46f7fb6904a58fb172b574cb15
jasonandjay <342690199@qq.com>
2022-06-22 17:39:25 +0800

    add css module

--- Project1/creation/src/pages/index.tsx
+++ Project1/creation/src/pages/index.tsx
@@ -1,6 +1,11 @@
-import styles from './index.less';
 import {useDispatch, useSelector, ArticleModelState} from 'umi';
 import {useEffect} from 'react';
+// css module的使用方式
+import styles from './index.less';
+// 可以在css module的基础上使用多个样式
+import classNames from 'classnames';
+// 禁止使用普通引入
+// import './index.less';
 
 export default function IndexPage() {
   const dispatch = useDispatch();
@@ -14,7 +19,9 @@ export default function IndexPage() {
 
   return (
     <div>
-      <h1 className={styles.title}>Page index</h1>
+      <h1 className={classNames(styles.title, styles.title2)}>Page index</h1>
+      {/* <h1 className="title">Page index</h1> */}
+
       {articleList.map(item=>{
         return <div key={item.id}>
           <p>{item.title}</p>
